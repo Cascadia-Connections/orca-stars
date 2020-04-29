0624d900bff25916fa0191c0fb657578d52f65c6
nateptay <natetay@outlook.com>
2020-04-29 16:33:32 -0700

    modifies controller/index view to pass business info to a confirmation page, which can read the data

--- OrcaStarsWebApplication/Controllers/AdminController.cs
+++ OrcaStarsWebApplication/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OrcaStarsWebApplication.Models;
 using OrcaStarsWebApplication.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -19,11 +20,17 @@ namespace OrcaStarsWebApplication.Controllers
         [HttpPost]
         public IActionResult Index(ApplicationViewModel avm)
         {
-            return RedirectToAction("Confirm", avm);
+            return RedirectToAction("Confirm", avm );
         }
 
         [HttpGet]
         public IActionResult Confirm(ApplicationViewModel avm)
+        {
+            return View(avm);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
         {
             return View();
         }
