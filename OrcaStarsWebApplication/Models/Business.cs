0508b66488b8c562e50cdf70b396ca76db3ea0ba
Madelyn Leahu <mackenzi.leahu@gmail.com>
2020-05-13 12:58:01 -0700

    Added Properties

--- OrcaStarsWebApplication/Models/Business.cs
+++ OrcaStarsWebApplication/Models/Business.cs
@@ -13,7 +13,22 @@ namespace OrcaStarsWebApplication.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public Point Coordinates { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        
+        //public SocialMedia SocMed { get; set; } add class, reference here
+        //public Hours Hours { get; set; }
         public string Hours { get; set; }
         public Bitmap Logo { get; set; } 
+        public Bitmap Store { get; set; }
+
+        //social media object - s.m data type as collection of s.m objects --TO DO
+        //research standard s.m data type --TO DO
+        //string address (add1, add2, city, state, zip) -- research standard --DONE
+        //storefront image of business (extra bitmap DT) --DONE
+        //business phone number?
     }
 }
