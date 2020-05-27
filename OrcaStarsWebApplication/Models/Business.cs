d2d0b4296e897b1ca3a494531331e9dad739b2b4
Madelyn Leahu <mackenzi.leahu@gmail.com>
2020-05-27 13:40:51 -0700

    bsuiness model

--- OrcaStarsWebApplication/Models/Business.cs
+++ OrcaStarsWebApplication/Models/Business.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Drawing;
 
-
 namespace OrcaStarsWebApplication.Models
 {
     public class Business
@@ -13,15 +12,18 @@ namespace OrcaStarsWebApplication.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public int PhoneNumber { get; set; }
-        public Point Coordinates { get; set; }
+        public Point Coordinates { get; set; } //change to geolocation not system.draw  
         public string Address1 { get; set; }
         public string Address2 { get; set; }
         public string City { get; set; }
-        public string State { get; set; }
+        public string State { get; set; } //if they have particular needs for the api and what theyre needs are 
         public int ZipCode { get; set; }
         public SocialMedia Social { get; set; } //collection or class
         public List<Hours> Hours { get; set; } //unsure of data collection type to use
         public Bitmap Logo { get; set; } 
         public Bitmap StoreFront { get; set; }
+        public Point Coordinates { get; set; }
+        public string Hours { get; set; }
+        public string Logo { get; set; } //File path reference
     }
 }
