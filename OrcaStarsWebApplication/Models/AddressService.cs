18e0e0a818944fe48f4c7d51c6eede7add74c046
nateptay <natetay@outlook.com>
2020-05-26 23:15:06 -0700

    adds map to confirmation page

---
+++ OrcaStarsWebApplication/Models/AddressService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrcaStarsWebApplication.Models
+{
+    public class AddressService
+    {
+        public string fieldsToAddress(string AddrL1, string AddrL2, string City, string State, string Country)
+        {
+            var address = "";
+            if (AddrL1 != null)
+                address += AddrL1;
+            if (AddrL2 != null)
+                address += AddrL2;
+            if (City != null)
+                address += City;
+            if (State != null)
+                address += State;
+            if (Country != null)
+                address += Country;
+            return address;
+        }
+    }
+}
